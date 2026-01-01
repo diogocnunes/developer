ae6bd9ab91957dab3ee5809814fcc40fd035fff2
Diogo Nunes <diogocnunes@gmail.com>
2026-01-01 17:34:48 +0000

    Change Settings

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'baseUrl' => 'http://localhost:8000',
-    'production' => false,
+    'baseUrl' => 'https://developer-vert.vercel.app',
+    'production' => true,
     'title' => 'Diogo Nunes - Certified Laravel Developer ',
     'description' => 'Senior Full-Stack Engineer specializing in high-security Laravel ecosystems. Building mission-critical solutions for government and global enterprises.',
     'collections' => [],
